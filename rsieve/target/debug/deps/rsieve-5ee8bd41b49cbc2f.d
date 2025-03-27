cf31060025708bf63fd77282d8a799cc27a90254
Roman-Zanotelli <redacted>
2025-03-27 09:41:38 -0400

    init rust program varient

---
+++ rsieve/target/debug/deps/rsieve-5ee8bd41b49cbc2f.d
@@ -0,0 +1,5 @@
+/home/roman/Desktop/Portfolio/LychrelSieve/rsieve/target/debug/deps/librsieve-5ee8bd41b49cbc2f.rmeta: src/main.rs
+
+/home/roman/Desktop/Portfolio/LychrelSieve/rsieve/target/debug/deps/rsieve-5ee8bd41b49cbc2f.d: src/main.rs
+
+src/main.rs:
---
+++ rsieve/target/debug/deps/rsieve-9facc977f9281f07.d
@@ -0,0 +1,5 @@
+/home/roman/Desktop/Portfolio/LychrelSieve/rsieve/target/debug/deps/librsieve-9facc977f9281f07.rmeta: src/main.rs
+
+/home/roman/Desktop/Portfolio/LychrelSieve/rsieve/target/debug/deps/rsieve-9facc977f9281f07.d: src/main.rs
+
+src/main.rs:
