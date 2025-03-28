d6ecef9b788e2d599c2b8e117f37dea53c023366
Roman-Zanotelli <redacted>
2025-03-28 12:07:23 -0400

    Scaffolding

--- rsieve/src/main.rs
+++ rsieve/src/main.rs
@@ -1,4 +1,4 @@
-
+pub mod number;
 fn main() {
     
 }
--- rsieve/target/debug/deps/rsieve-9facc977f9281f07.d
+++ rsieve/target/debug/deps/rsieve-9facc977f9281f07.d
@@ -1,5 +1,6 @@
-/home/roman/Desktop/Portfolio/LychrelSieve/rsieve/target/debug/deps/librsieve-9facc977f9281f07.rmeta: src/main.rs
+/home/roman/Desktop/Portfolio/LychrelSieve/rsieve/target/debug/deps/librsieve-9facc977f9281f07.rmeta: src/main.rs src/number.rs
 
-/home/roman/Desktop/Portfolio/LychrelSieve/rsieve/target/debug/deps/rsieve-9facc977f9281f07.d: src/main.rs
+/home/roman/Desktop/Portfolio/LychrelSieve/rsieve/target/debug/deps/rsieve-9facc977f9281f07.d: src/main.rs src/number.rs
 
 src/main.rs:
+src/number.rs:
